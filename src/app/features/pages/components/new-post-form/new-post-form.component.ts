e0934d43c38c478147708aed2da13d079a257701
Nikoloz Dvali <dvalin756@gmail.com>
2023-10-22 10:57:39 +0400

    took post form as a separate component

---
+++ src/app/features/pages/components/new-post-form/new-post-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-post-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-post-form.component.html',
+  styleUrls: ['./new-post-form.component.scss']
+})
+export class NewPostFormComponent {
+
+}
--- src/app/features/pages/components/posts/posts.component.ts
+++ src/app/features/pages/components/posts/posts.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClassroomService } from 'src/app/shared/services/classroom/classroom.service';
 import { IClass, IPost } from 'src/app/shared/interfaces';
+import { NewPostFormComponent } from '../new-post-form/new-post-form.component';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NewPostFormComponent],
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
