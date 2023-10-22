95b37e92da291beb0fbf7441b3e2853fec95371b
Nikoloz Dvali <dvalin756@gmail.com>
2023-10-22 22:20:27 +0400

    added students table

--- src/app/features/pages/components/students/students.component.ts
+++ src/app/features/pages/components/students/students.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClassroomService } from 'src/app/shared/services/classroom/classroom.service';
+import { Member, Owner } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-students',
@@ -8,6 +10,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.scss']
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit{
+  class_owner: Owner | undefined;
+  class_members: Member[] = [];
 
+  constructor(
+    private classroomService: ClassroomService
+  ){}
+
+  ngOnInit(){
+    this.classroomService.selectedClassData.subscribe(
+      (data)=>{
+        this.class_members = data?.class_members || [];
+        this.class_owner = data?.class_owner;
+      }
+    )
+  }
+
+  isOwner(member: Member){
+    return this.class_owner && member.member_id === this.class_owner.owner_id;
+  }
+  
 }
