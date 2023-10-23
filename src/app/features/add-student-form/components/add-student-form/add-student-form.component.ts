87576fd084fc2210fecb072f13c7f93b4396bcdb
Nikoloz Dvali <dvalin756@gmail.com>
2023-10-23 19:58:34 +0400

    added form builder

--- src/app/features/add-student-form/components/add-student-form/add-student-form.component.ts
+++ src/app/features/add-student-form/components/add-student-form/add-student-form.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-student-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-student-form.component.html',
   styleUrls: ['./add-student-form.component.scss']
 })
 export class AddStudentFormComponent {
 
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  newStudentForm = this.formBuilder.group({
+    studentId: ['', Validators.required]
+  })
 }
